91de12863c5fb1c459f24e9ef7d89603a6cce908
khuyen.tran <khuyen.tran@360life.vn>
2019-07-10 18:33:56 +0700

    next step login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
                 </div>
 
                 <div class="links">
-                    <a href="/login">Ligin</a>
+                    <a href="/login">Login</a>
                     <!-- <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
--- routes/web.php
+++ routes/web.php
@@ -13,79 +13,81 @@
 
 Auth::routes();
 
-// Route::get('/home', 'HomeController@index')->name('admin');
+Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'auth'], function () {
+        
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+
+    //  Test crud
+    Route::get('add','CarController@create');
+    Route::post('add','CarController@store');
+    Route::get('car','CarController@index');
+    Route::get('edit/{id}','CarController@edit');
+    Route::post('edit/{id}','CarController@update');
+    Route::delete('{id}','CarController@destroy');
+
+    Route::get('admin', function () {
+        return view('admin_template');
+    });
+
+    // option
+    Route::get('option/add','OptionController@create');
+    Route::post('option/add','OptionController@store');
+    Route::get('option','OptionController@index');
+    Route::get('option/edit/{id}','OptionController@edit');
+    Route::post('option/edit/{id}','OptionController@update');
+    Route::delete('option/{id}','OptionController@destroy');
+
+    // Route::get('api/v1/option/add','OptionController@apicreate');
+    // Route::post('api/v1/option/add','OptionController@apistore');
+    // Route::get('api/v1/option','OptionController@apiindex');
+    // Route::get('api/v1/option/edit/{id}','OptionController@apiedit');
+    // Route::post('api/v1/option/edit/{id}','OptionController@apiupdate');
+    // Route::delete('api/v1/option/{id}','OptionController@apidestroy');
+
+    // question
+    Route::get('question/add','QuestionController@create');
+    Route::post('question/add','QuestionController@store');
+    Route::get('question','QuestionController@index');
+    Route::get('question/edit/{id}','QuestionController@edit');
+    Route::post('question/edit/{id}','QuestionController@update');
+    Route::delete('question/{id}','QuestionController@destroy');
+    Route::get('question/search','QuestionController@search');
+
+    // Route::get('api/v1/question/add','QuestionController@apicreate');
+    // Route::post('api/v1/question/add','QuestionController@apistore');
+    // Route::get('api/v1/question','QuestionController@apiindex');
+    // Route::get('api/v1/question/edit/{id}','QuestionController@apiedit');
+    // Route::post('api/v1/question/edit/{id}','QuestionController@apiupdate');
+    // Route::delete('api/v1/question/{id}','QuestionController@apidestroy');
+    // Route::get('api/v1/question/search','QuestionController@apisearch');
+
+
+    // Voting
+    Route::get('voting/add','VotingController@create');
+    Route::post('voting/add','VotingController@store');
+    Route::get('voting','VotingController@index');
+    Route::get('voting/edit/{id}','VotingController@edit');
+    Route::post('voting/edit/{id}','VotingController@update');
+    Route::delete('voting/{id}','VotingController@destroy');
+    Route::get('voting/search','VotingController@search');
+
+    // Route::get('api/v1/voting/add','VotingController@apicreate');
+    // Route::post('api/v1/voting/add','VotingController@apistore');
+    // Route::get('api/v1/voting','VotingController@apiindex');
+    // Route::get('api/v1voting/edit/{id}','VotingController@apiedit');
+    // Route::post('api/v1/voting/edit/{id}','VotingController@apiupdate');
+    // Route::delete('api/v1/voting/{id}','VotingController@apidestroy');
+    // Route::get('api/v1/voting/search','VotingController@apisearch');
+
+    // Iframe
+    // Route::get('iframe/voting/{id}','VotingController@show');
+    // Route::get('iframe/question/{id}','QuestionController@show');
+
 });
-
-
-//  Test crud
-Route::get('add','CarController@create');
-Route::post('add','CarController@store');
-Route::get('car','CarController@index');
-Route::get('edit/{id}','CarController@edit');
-Route::post('edit/{id}','CarController@update');
-Route::delete('{id}','CarController@destroy');
-
-Route::get('home', function () {
-    return view('admin_template');
-});
-
-// option
-Route::get('option/add','OptionController@create');
-Route::post('option/add','OptionController@store');
-Route::get('option','OptionController@index');
-Route::get('option/edit/{id}','OptionController@edit');
-Route::post('option/edit/{id}','OptionController@update');
-Route::delete('option/{id}','OptionController@destroy');
-
-// Route::get('api/v1/option/add','OptionController@apicreate');
-// Route::post('api/v1/option/add','OptionController@apistore');
-// Route::get('api/v1/option','OptionController@apiindex');
-// Route::get('api/v1/option/edit/{id}','OptionController@apiedit');
-// Route::post('api/v1/option/edit/{id}','OptionController@apiupdate');
-// Route::delete('api/v1/option/{id}','OptionController@apidestroy');
-
-// question
-Route::get('question/add','QuestionController@create');
-Route::post('question/add','QuestionController@store');
-Route::get('question','QuestionController@index');
-Route::get('question/edit/{id}','QuestionController@edit');
-Route::post('question/edit/{id}','QuestionController@update');
-Route::delete('question/{id}','QuestionController@destroy');
-Route::get('question/search','QuestionController@search');
-
-// Route::get('api/v1/question/add','QuestionController@apicreate');
-// Route::post('api/v1/question/add','QuestionController@apistore');
-// Route::get('api/v1/question','QuestionController@apiindex');
-// Route::get('api/v1/question/edit/{id}','QuestionController@apiedit');
-// Route::post('api/v1/question/edit/{id}','QuestionController@apiupdate');
-// Route::delete('api/v1/question/{id}','QuestionController@apidestroy');
-// Route::get('api/v1/question/search','QuestionController@apisearch');
-
-
-// Voting
-Route::get('voting/add','VotingController@create');
-Route::post('voting/add','VotingController@store');
-Route::get('voting','VotingController@index');
-Route::get('voting/edit/{id}','VotingController@edit');
-Route::post('voting/edit/{id}','VotingController@update');
-Route::delete('voting/{id}','VotingController@destroy');
-Route::get('voting/search','VotingController@search');
-
-// Route::get('api/v1/voting/add','VotingController@apicreate');
-// Route::post('api/v1/voting/add','VotingController@apistore');
-// Route::get('api/v1/voting','VotingController@apiindex');
-// Route::get('api/v1voting/edit/{id}','VotingController@apiedit');
-// Route::post('api/v1/voting/edit/{id}','VotingController@apiupdate');
-// Route::delete('api/v1/voting/{id}','VotingController@apidestroy');
-// Route::get('api/v1/voting/search','VotingController@apisearch');
-
-// Iframe
-// Route::get('iframe/voting/{id}','VotingController@show');
-// Route::get('iframe/question/{id}','QuestionController@show');
-
-
 
 
